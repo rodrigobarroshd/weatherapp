d258ddd6b4036ade09e4b2c29a8145fe8da13689
Rodrigo Barros <brodrigo87@gmail.com>
2022-12-16 15:51:21 -0300

    app

---
+++ appplant/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>App Plant</Text>
+      <Image source={require('./assets/jadeplant.png')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
