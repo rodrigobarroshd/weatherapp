8667876553c78e687be98d83a29715dc4a5bdf71
Rodrigo Barros <brodrigo87@gmail.com>
2022-12-19 15:13:36 -0300

    mock

---
+++ appplant/constants/mocks.js
@@ -0,0 +1,63 @@
+const categories = [
+    {
+        id: 1,
+        name : 'Plants',
+        name : ['products','inspirations'],
+        count: 147,
+        image: require('../assets/images/plants.png')
+    },
+    {
+        id: 'seeds',
+        name : 'Seeds',
+        name : ['products','shop'],
+        count: 147,
+        image: require('../assets/images/seeds.png')
+    },
+    {
+        id: 'flowers',
+        name : 'Flowers',
+        name : ['products', 'inspirations'],
+        count: 16,
+        image: require('../assets/images/flowers.png')
+    },
+    {
+        id: 'sprayers',
+        name : 'Sprayers',
+        name : ['products', 'shop'],
+        count: 17,
+        image: require('../assets/images/sprayers.png')
+    },
+    {
+        id: 'pots',
+        name : 'Pots',
+        name : ['products', 'inspirations'],
+        count: 47,
+        image: require('../assets/images/pots.png')
+    },
+    {
+        id: 'fertilizers',
+        name : 'Fertilizers',
+        name : ['products', 'inspirations'],
+        count: 47,
+        image: require('../assets/images/fertilizers.png')
+    },
+
+];
+
+const products = [
+
+];
+
+const explore = [
+    // images
+];
+
+const profile = {
+
+};
+
+export {
+    categories,
+    products,
+    profile,
+}
